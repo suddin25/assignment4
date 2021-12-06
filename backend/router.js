d067e33a6901753ae67313896d88001634e1a6c2
SaifU213 <55818476+suddin25@users.noreply.github.com>
2021-12-05 21:16:04 -0500

    Git Ignore Added

--- backend/router.js
+++ backend/router.js
@@ -4,12 +4,45 @@ const app = express();
 
 const newConnection = require("./DBConnection");
 
-app.get("/users", function (req, res) {
+app.get("/users", (req, res) => {
 	let conn = newConnection();
 	conn.connect();
 
-	conn.query("SELECT * from users", function (error, results, fields) {
-		if (error) throw error;
-		res.send(JSON.stringify(results));
-	});
+	let response = "";
+
+	conn.query("SELECT username FROM users", 
+	(error, res) => {
+		if (error) {
+			console.log(error)
+		}
+		else {
+			res.send(JSON.stringify(results));
+		}
+	}	
+});
+
+app.post("/insert-new-user", (req, res) => {
+	let conn = newConnection();
+	conn.connect();
+
+	const username = req.body.username;
+	const userPassword = req.body.userPassword;
+	const gender = req.body.gender;
+	const age = req.body.age;
+
+	conn.query(
+		"INSERT INTO users (username, userPassword, gender, age) VALUES (?,?,?,?)",
+		[username, userPassword, gender, age],
+		(error, result) => {
+			if (error) {
+				console.log(error);
+			} else {
+				res.send("New user added");
+			}
+		}
+	);
+});
+
+app.listen(3001, () => {
+	console.log("Your server is running on port 3001");
 });
