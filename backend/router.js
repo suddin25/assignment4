86cc92b46572241c685fe716db3a272e0ee51cc2
suddin25 <suddin25@uwo.ca>
2021-12-07 00:23:23 -0500

    Final Commit

--- backend/router.js
+++ backend/router.js
@@ -3,26 +3,13 @@ const app = express();
 const port = 3200;
 
 const newConnection = require("./DBConnection");
-//const cors = require("cors");
+const cors = require("cors");
 
-//app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
 	res.send("Hello World!");
-});
-
-app.get("/get-users", (req, res) => {
-	let conn = newConnection();
-	conn.connect();
-
-	conn.query("SELECT username FROM users", (error, result) => {
-		if (error) {
-			console.log(error);
-		} else {
-			res.send(result);
-		}
-	});
 });
 
 app.post("/add-user", (req, res) => {
@@ -41,9 +28,106 @@ app.post("/add-user", (req, res) => {
 		[username, userPassword, gender, age],
 		(error, result) => {
 			if (error) {
-				console.log(error);
+				if (error.code == "ER_DUP_ENTRY" || error.errno == 1062) {
+					console.log("Duplicate key");
+					res.send("Duplicate");
+				} else {
+					console.log(error);
+					res.send("Other Error");
+				}
 			} else {
 				res.send("New user inserted");
+			}
+		}
+	);
+});
+
+app.post("/update-password", (req, res) => {
+	let conn = newConnection();
+	conn.connect();
+
+	let username = req.body.username;
+	let userPassword = req.body.userPassword;
+
+	conn.query(
+		`UPDATE users SET userPassword = "${userPassword}" WHERE userName = "${username}"`,
+		(error, result) => {
+			if (error) {
+				console.log(error);
+				res.send("Error Occured");
+			} else {
+				res.send("New password set");
+			}
+		}
+	);
+});
+
+app.get("/top-5-albums", (req, res) => {
+	let conn = newConnection();
+	conn.connect();
+
+	conn.query(
+		"SELECT a.artistUsername, aal.albumId, r.albumID, r.rating FROM artist a, artistalbum aal, reviewsandrating r WHERE a.artistUsername = aal.artistUsername AND r.albumID = aal.albumID ORDER BY r.rating DESC LIMIT 5",
+		(error, result) => {
+			if (error) {
+				console.log(error);
+			} else {
+				res.send(result);
+			}
+		}
+	);
+});
+
+app.get("/user-get-songs", (req, res) => {
+	let conn = newConnection();
+	conn.connect();
+
+	let username = req.query.username;
+
+	conn.query(
+		`SELECT s.songName FROM songs s, usersongs us WHERE s.songID = us.songID AND us.username = "${username}"`,
+		(error, result) => {
+			if (error) {
+				console.log(error);
+			} else {
+				res.send(result);
+			}
+		}
+	);
+});
+
+app.post("/delete-user-songs", (req, res) => {
+	let conn = newConnection();
+	conn.connect();
+
+	let username = req.body.username;
+
+	conn.query(
+		`DELETE FROM usersongs WHERE userName="${username}"`,
+		(error, result) => {
+			if (error) {
+				console.log(error);
+				res.send("Error Occured");
+			} else {
+				res.send("User Playlist Deleted");
+			}
+		}
+	);
+});
+
+app.get("/get-album-songs", (req, res) => {
+	let conn = newConnection();
+	conn.connect();
+
+	let albumName = req.query.albumName;
+
+	conn.query(
+		`SELECT s.songName FROM songs s, album a WHERE s.albumID = a.albumID AND a.albumName = "${albumName}"`,
+		(error, result) => {
+			if (error) {
+				console.log(error);
+			} else {
+				res.send(result);
 			}
 		}
 	);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Axios from "axios";
 
 function App() {
@@ -8,15 +8,6 @@ function App() {
 	const [gender, setGender] = useState("");
 	const [age, setAge] = useState("");
 
-	//const [usersList, setUsersList] = useState([]);
-
-	/* 	useEffect(() => {
-		fetch("/");
-		return () => {
-			cleanup;
-		};
-	}, []); */
-
 	const addUser = () => {
 		Axios.post("http://localhost:3200/add-user", {
 			username: username,
@@ -24,19 +15,88 @@ function App() {
 			gender: gender,
 			age: age,
 		}).then(() => {
-			console.log("success");
+			alert("User Added");
 		});
 	};
 
-	const getUsers = () => {
-		Axios.get("http://localhost:3200/get-users").then((response) => {
+	// F1
+	let topAlbumsList = "";
+
+	const getTopFiveArtists = () => {
+		Axios.get("http://localhost:3200/top-5-albums").then((response) => {
+			response.data.forEach((element) => {
+				topAlbumsList = topAlbumsList + element.artistUsername + "\n";
+			});
 			console.log(response);
+			alert(topAlbumsList);
+		});
+	};
+
+	const [usernameNewPass, setUsernameNewPass] = useState("");
+	const [newPass, setNewPass] = useState("");
+
+	// F2
+	const updatePassword = () => {
+		Axios.post("http://localhost:3200/update-password", {
+			username: usernameNewPass,
+			userPassword: newPass,
+		}).then(() => {
+			alert(`Password updated for ${usernameNewPass}`);
+		});
+	};
+
+	const [albumName, setAlbumName] = useState("");
+
+	// F3
+	let albumSongsList = "";
+
+	const getAlbumSongs = () => {
+		Axios.get("http://localhost:3200/get-album-songs", {
+			params: {
+				albumName: albumName,
+			},
+		}).then((response) => {
+			response.data.forEach((element) => {
+				albumSongsList = albumSongsList + element.songName + "\n";
+			});
+			console.log(response);
+			alert(albumSongsList);
+		});
+	};
+
+	// F4
+	const [usernameGetSongs, setUsernameGetSongs] = useState("");
+	let songList = "";
+
+	const getUserSongs = () => {
+		Axios.get("http://localhost:3200/user-get-songs", {
+			params: {
+				username: usernameGetSongs,
+			},
+		}).then((response) => {
+			response.data.forEach((element) => {
+				songList = songList + element.songName + "\n";
+			});
+			console.log(response);
+			alert(songList);
+		});
+	};
+
+	// F5
+	const [usernameDeletePlaylist, setUsernameRemovePlaylist] = useState("");
+
+	const deleteAllUserSongs = () => {
+		Axios.post("http://localhost:3200/delete-user-songs", {
+			username: usernameDeletePlaylist,
+		}).then(() => {
+			alert("All Songs Removed");
 		});
 	};
 
 	return (
 		<div className="App">
-			<div className="createUser">
+			<div className="continer">
+				<h3>Create User</h3>
 				<label>Username:</label>
 				<input
 					type="text"
@@ -65,20 +125,72 @@ function App() {
 						setAge(event.target.value);
 					}}
 				/>
-				<button onClick={addUser}>Add Employee</button>
+				<button onClick={addUser}>Add User</button>
 			</div>
-			{/* <div className="employees"></div>
-			<div className="Login">
-				<button
-					className="Users"
-					onClick={() => {
-						fetch("/users");
+
+			<div className="continer">
+				<h3>Update Password</h3>
+				<label>Enter Username:</label>
+				<input
+					type="text"
+					onChange={(event) => {
+						setUsernameNewPass(event.target.value);
 					}}
-				>
-					Users
-				</button>
+				/>
+				<label>Enter New Password:</label>
+				<input
+					type="text"
+					onChange={(event) => {
+						setNewPass(event.target.value);
+					}}
+				/>
+				<button onClick={updatePassword}>Update Password</button>
 			</div>
-			<label htmlFor=""></label> */}
+
+			<div className="continer">
+				<h3>Get list of User Songs</h3>
+				<label>Enter Username:</label>
+				<input
+					type="text"
+					onChange={(event) => {
+						setUsernameGetSongs(event.target.value);
+					}}
+				/>
+				<button onClick={getUserSongs}>Get User Songs</button>
+				<div className="songs"></div>
+			</div>
+
+			<div className="continer">
+				<h3>Remove All Of User's Songs</h3>
+				<label>Enter Username:</label>
+				<input
+					type="text"
+					onChange={(event) => {
+						setUsernameRemovePlaylist(event.target.value);
+					}}
+				/>
+				<button onClick={deleteAllUserSongs}>Remove Playlist</button>
+			</div>
+
+			<div className="continer">
+				<h3>Get Top 5 Artists</h3>
+				<button onClick={getTopFiveArtists}>
+					Get Top Five Artists
+				</button>
+				<div>{topAlbumsList}</div>
+			</div>
+
+			<div className="continer">
+				<h3>Get Songs In A Album</h3>
+				<label>Enter Album Name:</label>
+				<input
+					type="text"
+					onChange={(event) => {
+						setAlbumName(event.target.value);
+					}}
+				/>
+				<button onClick={getAlbumSongs}>Get Songs From Album</button>
+			</div>
 		</div>
 	);
 }
